8cebb23fde94c166c50ebe18656657a0ad6e8e6a
chuRofu <churofu@chuRiris-PC.local>
2019-09-11 16:47:24 +0900

    Create MemoList

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
 
       <View style={styles.appbar}>
         <View>
-          <Text>MEMOT</Text>
+          <Text style={styles.appbarTitle}>MEMOT</Text>
         </View>
       </View>
 
@@ -16,38 +16,30 @@ export default function App() {
           <Text style={styles.memoTitle}>講座のアイデア</Text>
           <Text style={styles.memoDate}>2017/10/10</Text>
         </View>
-      </View>
 
-      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2017/10/10</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2017/10/10</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2017/10/10</Text>
+        </View>
+
         <View style={styles.memoListItem}>
           <Text style={styles.memoTitle}>講座のアイデア</Text>
           <Text style={styles.memoDate}>2017/10/10</Text>
         </View>
       </View>
 
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイデア</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイデア</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイデア</Text>
-          <Text style={styles.memoDate}>2017/10/10</Text>
-        </View>
-      </View>
-
-      <View>
-        <Text>+</Text>
+      <View style={styles.memoAddButton}>
+        <Text style={styles.memoAddButtonTitle}>+</Text>
       </View>
     </View>
   );
@@ -56,8 +48,67 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
+  },
+  memoList: {
+    flex: 1,
+    width: '100%',
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
