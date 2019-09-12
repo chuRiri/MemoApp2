1ef1ffd251e7b3a7d7d3b6ab1de964bc0ce4331b
chuRofu <churofu@chuRiris-PC.local>
2019-09-12 18:00:59 +0900

    Import react-navigation

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Appbar />
-        <SignupScreen />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFDF6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
+const App = createStackNavigator({
+  Home:       { screen: MemoListScreen },
+  Login:      { screen: LoginScreen },
+  Signup:     { screen: SignupScreen },
+  MemoDetail: { screen: MemoDetailScreen },
+  MemoEdit:   { screen: MemoEditScreen },
+}, {
+  defaultNavigationOptions: {
+    headerTitle: 'Memot',
+    headerStyle: {
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 2,
+      backgroundColor: '#265366',
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
   },
 });
+
+export default createAppContainer(App);
