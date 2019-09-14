e36541aea24080565c22c191e567c0975718d76c
chuRofu <churofu@chuRiris-PC.local>
2019-09-14 17:00:50 +0900

    メモ登録処理(固定)

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ import SignupScreen from './src/screens/SignupScreen';
 
 import ENV from './env.json';
 
+// eslint-disable-next-line
+require("firebase/firestore");
+
 const config = {
   apiKey:            ENV.FIREBASE_API_KEY,
   authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
--- src/screens/MemoListScreen.js
+++ src/screens/MemoListScreen.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import firebase from 'firebase';
+
 import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
 class MemoListScreen extends React.Component {
+  // eslint-disable-next-line
+  handlePress() {
+    const db = firebase.firestore();
+    const { params } = this.props.navigation.state;
+    // db.settings({ timestampsInSnapshots: true });
+    // db.collection(`users/${params.currentUser.uid}/memos`).add({
+    const { currentUser } = firebase.auth();
+    db.collection(`users/${currentUser.uid}/memos`).add({
+      body: 'Hello',
+      createdOn: '2017-12-12',
+    })
+      .then((docRef) => {
+        console.log(docRef.id);
+      })
+      .catch((error) => {
+        console.log(eeror);
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MemoList navigation={this.props.navigation} />
-        <CircleButton name="plus" onPress={() => { this.props.navigation.navigate('MemoEdit'); }} />
+        <CircleButton name="plus" onPress={this.handlePress.bind(this)} />
       </View>
     );
   }
