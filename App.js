0e50b377fbac68be06e723eab5bd9ec93879aa4e
chuRofu <churofu@chuRiris-PC.local>
2019-09-14 11:02:17 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -7,15 +8,28 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+const config = {
+  apiKey: 'AIzaSyBe1BF7efCY-p4FWGVSvtA4BqfL7bvOL4k',
+  authDomain: 'memoapp-16d57.firebaseapp.com',
+  databaseURL: 'https://memoapp-16d57.firebaseio.com',
+  projectId: 'memoapp-16d57',
+  storageBucket: '',
+  messagingSenderId: '282840975178',
+  appId: '1:282840975178:web:7499d60f8aa964bce7a3b3',
+};
+firebase.initializeApp(config);
+
 const App = createStackNavigator({
-  Home:       { screen: MemoListScreen },
   Login:      { screen: LoginScreen },
+  Home:       { screen: MemoListScreen },
   Signup:     { screen: SignupScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'Memot',
+    headerTintColor: '#fff',
+    headerBackTitle: null,
     headerStyle: {
       shadowColor: '#000',
       shadowOffset: { width: 0, height: 2 },
