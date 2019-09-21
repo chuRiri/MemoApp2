740b3464901f01b0dc6eec6f5e3dff829c1bb980
chuRiri <churofu@gmail.com>
2019-09-21 22:19:07 +0900

    キーボード表示でボタンが隠れる不具合を修正

--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 
 import firebase from 'firebase';
 
@@ -26,17 +26,16 @@ class MemoCreateScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
         <TextInput
           style={styles.memoEditInput}
           multiline
           value={this.state.body}
           onChangeText={(text) => { this.setState({ body: text }); }}
           textAlignVertical="top"
-          returnKeyType="done"
         />
         <CircleButton name="check" onPress={this.handlePress.bind(this)} />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
